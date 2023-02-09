bbdc7d4176d0aab9a8750d2969572f221010d6e8
KiKiKi <chaika.design@gmail.com>
2023-02-10 00:58:25 +0900

    :unicorn: Implement to return localization message from json file.
    cf. https://github.com/RinkAttendant6/JsonI18n/blob/master/src/Translate.php

--- src/Translation.php
+++ src/Translation.php
@@ -15,6 +15,48 @@ class Translation {
   public function __construct(string $locale = 'en-US', string $file = 'messages.json') {
     $resource = new Resource($locale, $file);
     $this->data = $resource->getData();
-    var_dump($this->data);
+  }
+
+  /**
+   * @param string $key: The key of localized text (json)
+   * @param string|array $placeholders: The text to be replaced within the localized text
+   */
+  public function __(string $key, $placeholders = null): string {
+    // copy array
+    $data = $this->data;
+
+    $keys = explode('.', $key);
+
+    $message = array_reduce($keys, function(array $carry, string $key) {
+      if ( !isset($carry[$key]) ) {
+        // throw new \OutOfBoundsException("Invalid key: '{$key}'");
+        return $carry;
+      }
+
+      return $carry[$key];
+    }, $data);
+
+    if ( is_array($message) ) {
+      throw new \OutOfBoundsException("Invalid key: '{$key}'");
+    }
+
+    if ( is_null($placeholders) ) {
+      return $message;
+    }
+
+    // replace placeholder
+    if ( is_array($placeholders) ) {
+      return vsprintf($message, $placeholders);
+    }
+
+    if ( is_string($placeholders) || is_float($placeholders) || is_int($placeholders) ) {
+      return sprintf($message, $placeholders);
+    }
+
+    throw new InvalidArgumentException('Placeholders have to be a string or array to return a formatted localized message');
+  }
+
+  public function __e(string $key, $placeholders = null): void {
+    echo $this->__($key, $placeholders);
   }
 }
